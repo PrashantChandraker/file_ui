f523e2a2a236d81ac848384fee93655b146ec64c
PrashantChandraker <prashantchandraker1@gmail.com>
2024-11-05 12:53:38 +0530

    completed

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:file_ui/filemanager_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 21, 20, 20),
+      ), // Dark theme for a similar look
+      home: const FileManagerScreen(),
+    );
+  }
+}
